033655562a07bab0bea7710ba1872b6ea01f2840
Sahil Yadav <sahilyadavc69@gmail.com>
2025-10-06 00:03:48 +0530

    mic_test.py

--- Code/mic_test.py
+++ Code/mic_test.py
@@ -1,13 +1,11 @@
 import speech_recognition as sr
-# Initialize recognizer
+
 r = sr.Recognizer()
 
-# Capture microphone input
 with sr.Microphone() as source:
     print("Say something...")
     audio = r.listen(source)
 
-# Recognize speech using Google API
 try:
     text = r.recognize_google(audio)
     print("You said:", text)
