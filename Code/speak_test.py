d94947823824d69f6698cd3708e3c08ebc9cbdf2
Sahil Yadav <sahilyadavc69@gmail.com>
2025-10-06 00:03:22 +0530

    speak_test.py

--- Code/speak_test.py
+++ Code/speak_test.py
@@ -1,10 +1,3 @@
-# import pyttsx3
-
-# engine = pyttsx3.init()
-
-# engine.say("Hello! I am your desktop assistant. I can talk now.")
-# engine.runAndWait()
-
 import pyttsx3
 
 engine = pyttsx3.init()
@@ -13,7 +6,7 @@ voices = engine.getProperty('voices')
 for idx, voice in enumerate(voices):
     print(idx, voice.id)
 
-# pick one voice
+
 engine.setProperty('voice', voices[0].id)
 engine.say("Hello! This is a voice test.")
 engine.runAndWait()
